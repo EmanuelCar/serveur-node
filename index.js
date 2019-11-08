d7e66f84a530b2e7f375bb64796d8500ce02d808
Lapiochebasic11 <lilianschneider17@gmail.com>
2019-11-08 10:17:55 +0100

    index.marche

--- index.js
+++ index.js
@@ -1,18 +1,74 @@
+
+var express = require('express');
+
 //Load HTTP module
 const http = require("http");
 const hostname = '127.0.0.1';
 const port = 3000;
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+var app = express();
 
-    //Set the response HTTP header with HTTP status and Content type
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+//Afin de faciliter le routage (les URL que nous souhaitons prendre en charge dans notre API), nous créons un objet Router.
+//C'est à partir de cet objet myRouter, que nous allons implémenter les méthodes. 
+var myRouter = express.Router();
+
+// Je vous rappelle notre route (/piscines).  
+myRouter.route('/')
+    // all permet de prendre en charge toutes les méthodes. 
+    .all(function (req, res) {
+        res.json({ message: "Bienvenue sur notre Frugal API ", methode: req.method });
+    });
+
+myRouter.route('/piscines')
+    // J'implémente les méthodes GET, PUT, UPDATE et DELETE
+    // GET
+    .get(function (req, res) {
+        res.json({
+            message: "Liste les piscines de Lille Métropole avec paramètres :",
+            ville: req.query.ville,
+            nbResultat: req.query.maxresultat,
+            methode: req.method
+        });
+    })
+    //POST
+    .post(function (req, res) {
+        res.json({
+            message: "Ajoute une nouvelle piscine à la liste",
+            nom: req.body.nom,
+            ville: req.body.ville,
+            taille: req.body.taille,
+            methode: req.method
+        });
+    })
+    //PUT
+    .put(function (req, res) {
+        res.json({ message: "Mise à jour des informations d'une piscine dans la liste", methode: req.method });
+    })
+    //DELETE
+    .delete(function (req, res) {
+        res.json({ message: "Suppression d'une piscine dans la liste", methode: req.method });
+    });
+
+// Nous demandons à l'application d'utiliser notre routeur
+app.use(myRouter);
+
+// Démarrer le serveur 
+app.listen(port, hostname, function () {
+    console.log("Mon serveur fonctionne sur http://" + hostname + ":" + port);
 });
 
-//listen for request on port 80, and as a callback function have the port listened on logged
-server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+myRouter.route('/piscines/:piscine_id')
+    .get(function (req, res) {
+        res.json({ message: "Vous souhaitez accéder aux informations de la piscine n°" + req.params.piscine_id });
+    })
+    .put(function (req, res) {
+        res.json({ message: "Vous souhaitez modifier les informations de la piscine n°" + req.params.piscine_id });
+    })
+    .delete(function (req, res) {
+        res.json({ message: "Vous souhaitez supprimer la piscine n°" + req.params.piscine_id });
+    });

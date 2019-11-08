5fba39455dd2434de91c1fc5f9e1aa48ccd6da73
Gougoutte <emanuel.caracciolo@viacesi.fr>
2019-11-08 09:24:23 +0100

    premire commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const credentials = {
+    client: {
+        id: '<client-id>',
+        secret: '<client-secret>'
+    },
+    auth: {
+        tokenHost: 'https://api.oauth.com'
+    }
+};
+
+// Initialize the OAuth2 Library
+const oauth2 = require('simple-oauth2').create(credentials);

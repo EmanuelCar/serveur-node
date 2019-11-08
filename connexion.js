242dd1d21b8f45b4f5e972ee71588bd1803bf89c
Gougoutte <emanuel.caracciolo@viacesi.fr>
2019-11-08 09:56:01 +0100

    premier commit

--- connexion.js
+++ connexion.js
@@ -1,12 +1,30 @@
+var express = require('express');
 var mysql = require('mysql');
+var app = express();
 
-var con = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: ""
+    password: "",
+    database: "projet_web"
 });
 
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connecté!");
+connection.connect(function (error) {
+    if (!!error){
+    console.log("Erreur");
+    } else {
+        console.log("Connecté !");
+    }
 });
+
+app.get('/', function(req, resp){
+    connection.query("SELECT * FROM utilisateurs", function(error, rows, fields){
+        if(!!error) {
+            console.log('Erreur dans la requête');
+        } else {
+            console.log('Requête réussie !');
+        }
+    });
+})
+
+app.listen(1337);
